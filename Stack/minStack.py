6b00024f01e78486c36bb3fbe5758b595134dcca
Shraddha Piparia <shraddhapiparia@gmail.com>
2020-03-13 10:49:07 -0500

    Update minStack.py

--- Stack/minStack.py
+++ Stack/minStack.py
@@ -30,3 +30,41 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+
+#Order of 1 for getmin
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.items = []
+        self.min = []
+
+    def push(self, x: int) -> None:
+        self.items.append(x)
+        if not self.min or x <= self.min[-1]:
+            self.min.append(x)
+
+    def pop(self) -> None:
+        if not self.items:
+            return None
+        
+        item = self.items.pop()
+        
+        if item == self.min[-1]:
+            self.min.pop() 
+        
+        return item 
+    
+    def top(self) -> int:
+        if not self.items:
+            return None
+        return self.items[-1]
+
+    def getMin(self) -> int:
+        if not self.min: 
+            return None
+        return self.min[-1]
