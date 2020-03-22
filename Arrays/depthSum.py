2fd7e073a0f656be121993071b363b7bc5b33d5f
Shraddha Piparia <shraddhapiparia@gmail.com>
2020-03-22 16:57:26 -0500

    339. Nested List Weight Sum
    Given a nested list of integers, return the sum of all integers in the list weighted by their depth. Each element is either an integer, or a list -- whose elements may also be integers or other lists.

---
+++ Arrays/depthSum.py
@@ -0,0 +1,61 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        summ = out = 0
+        level = 1
+        if len(nestedList) == 0:
+            return 0
+        out = self.check(nestedList, level)
+        return out
+        
+    def check(self, l: List[NestedInteger], level : int) -> int:
+        summ = 0
+        for val in l:
+            v = val.getInteger()
+            if not v:
+                summ += self.check(val.getList(),level+1)
+            else:
+                summ += level*v
+        return summ
