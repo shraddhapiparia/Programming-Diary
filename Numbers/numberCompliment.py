f5569dc2ad49ee6420d94326c51102b28db8f521
Shraddha Piparia <shraddhapiparia@gmail.com>
2020-05-04 09:36:04 -0500

    Update numberCompliment.py

--- Numbers/numberCompliment.py
+++ Numbers/numberCompliment.py
@@ -1,4 +1,4 @@
-# Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+# 476. Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
 class Solution:
     def findComplement(self, num: int) -> int:
         quo, rem, s = num//2, num%2, 0
