08dcd66fefa02fb43a677b4998b9ac363370fcb1
Shraddha Piparia <shraddhapiparia@gmail.com>
2020-10-26 18:32:12 -0500

    Create subdomainVisits.py

---
+++ Strings/subdomainVisits.py
@@ -0,0 +1,31 @@
+# 811
+# A website domain like "discuss.leetcode.com" consists of various subdomains. At the top level, we have "com", at the next level, we have "leetcode.com", and 
+# at the lowest level, "discuss.leetcode.com". When we visit a domain like "discuss.leetcode.com", we will also visit the parent domains "leetcode.com" and "com" 
+# implicitly. Now, call a "count-paired domain" to be a count (representing the number of visits this domain received), followed by a space, followed by the address. 
+# An example of a count-paired domain might be "9001 discuss.leetcode.com".
+
+# We are given a list cpdomains of count-paired domains. We would like a list of count-paired domains, (in the same format as the input, and in any order), that 
+# explicitly counts the number of visits to each subdomain.
+
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        dic, out = {}, []
+        for values in cpdomains:
+            num = values.split()[0]
+            domain = values.split()[1]
+            if domain in dic:
+                dic[domain] += int(num)
+            else:
+                dic[domain] = int(num)
+            count= domain.count(".")
+            for i in range(count):
+                part = domain[domain.index(".")+1:]
+                if part in dic:
+                    dic[part] += int(num)
+                else:
+                    dic[part] = int(num)
+                domain = part
+        for site,values in dic.items():
+            out.append(str(values)+" " + str(site))
+        return out
+                
