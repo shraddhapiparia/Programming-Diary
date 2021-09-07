ace0563b4888fbcce9f456923a30c959d83b0370
Shraddha Piparia <shraddhapiparia@gmail.com>
2021-09-07 10:29:15 -0500

    Update canConstruct.py

--- Strings/canConstruct.py
+++ Strings/canConstruct.py
@@ -7,6 +7,28 @@ class Solution:
                 if c not in magazine or ransomNote.count(c) > magazine.count(c):
                     return False
         return True
+    
+# longer code
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine):
+            return False
+        ransom_dict, magazine_dict = {}, {}
+        for r in ransomNote:
+            if r not in ransom_dict:
+                ransom_dict[r] = 1
+            else:
+                ransom_dict[r] += 1
+        for m in magazine:
+            if m not in magazine_dict:
+                magazine_dict[m] = 1
+            else:
+                magazine_dict[m] += 1
+        for ch, count in ransom_dict.items():
+            if ch not in magazine_dict or magazine_dict[ch] < count:
+                return False
+        return True
         
 # 383 Leetcode        
 #  Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
