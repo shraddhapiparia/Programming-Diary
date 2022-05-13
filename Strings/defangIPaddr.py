109dff73a386821c1e4a61f6b9209223f36691f9
Shraddha Piparia <shraddhapiparia@gmail.com>
2022-05-13 13:04:28 -0700

    Update defangIPaddr.py

--- Strings/defangIPaddr.py
+++ Strings/defangIPaddr.py
@@ -1,6 +1,11 @@
-# 1108. Defanging an IP Address. Given a valid (IPv4) IP address, return a defanged version of that IP address.
+# 1108. Defanging an IP Address. Given a valid (IPv4) IP address, return a defanged version of that IP address. Easy
 
 # A defanged IP address replaces every period "." with "[.]".
+
+# Example 1:
+
+# Input: address = "1.1.1.1"
+# Output: "1[.]1[.]1[.]1"
 
 class Solution:
     def defangIPaddr(self, address: str) -> str:
@@ -11,3 +16,9 @@ class Solution:
             else:
                 new += address[i]
         return new
+    
+ # One liner
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace('.','[.]')
