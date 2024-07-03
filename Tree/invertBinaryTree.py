f9a3f7c8165bed4b81475678be367e02b27fe5ec
Shraddha Piparia <shraddhapiparia@gmail.com>
2024-07-03 15:30:35 -0700

    Update invertBinaryTree.py
    Added recursive solution

--- Tree/invertBinaryTree.py
+++ Tree/invertBinaryTree.py
@@ -37,3 +37,13 @@ class Solution:
                 node.left, node.right = node.right, node.left
                 stack += node.left, node.right
         return root
+
+# Using recursion
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
