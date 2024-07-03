45840715d8102558d51a86ba47abe6eb41677b79
Shraddha Piparia <shraddhapiparia@gmail.com>
2024-07-03 13:55:21 -0700

    Update maxDepthBinaryTree.py
    Maximum depth of binary tree

--- Tree/maxDepthBinaryTree.py
+++ Tree/maxDepthBinaryTree.py
@@ -19,3 +19,13 @@ class Solution:
             if n.right:
                 stack.append((n.right, depth+1))
         return maxval
+
+# Using recursion
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+
+        return max(left_depth, right_depth) + 1
