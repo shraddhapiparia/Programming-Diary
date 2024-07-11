02ef23d787fec008026216477cd5e08aa0ee8b96
Shraddha Piparia <shraddhapiparia@gmail.com>
2024-07-11 09:02:53 -0700

    Create convertZigZag.py

---
+++ Strings/convertZigZag.py
@@ -0,0 +1,24 @@
+# 6. Zigzag Conversion
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# And then read line by line: "PAHNAPLSIIGYIR"
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        idx, d = 0, 1
+        rows = [[] for _ in range(numRows)]
+        for char in s:
+            rows[idx].append(char)
+            if idx == 0:
+                d = 1
+            elif idx == numRows-1:
+                d = -1
+            idx += d
+        for i in range(numRows):
+            rows[i] = ''.join(rows[i])
+        return ''.join(rows)
+
+# TC - O(N)
