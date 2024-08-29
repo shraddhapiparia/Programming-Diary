2c05d7acb270ce3615d0b39e4ed7d814f570da6a
Shraddha Piparia <shraddhapiparia@gmail.com>
2024-08-28 17:27:59 -0700

    Update romanToInt.py

--- Numbers/romanToInt.py
+++ Numbers/romanToInt.py
@@ -1,18 +1,13 @@
 #13. Roman to Integer
 
+# Replace condensed values to original form
 class Solution:
     def romanToInt(self, s: str) -> int:
-        hashmap = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        i = 0
-        while i < len(s):
-            ss = 0
-            if i+1 < len(s) and hashmap[s[i+1]] > hashmap[s[i]]:
-                ss = hashmap[s[i+1]] - hashmap[s[i]]
-                i += 2
-            else:
-                ss = hashmap[s[i]]
-                i += 1
-            val += ss
-        return val
-        
-# Time and Space Complexity: O(1)
+        val = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += val[char]
+        return number
