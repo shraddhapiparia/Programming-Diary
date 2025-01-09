5094c5567f0e99cd7d1d68fc227f6ed72d651594
Shraddha Piparia <shraddhapiparia@gmail.com>
2025-01-08 16:46:54 -0800

    Update triangularSum.py

--- Arrays/triangularSum.py
+++ Arrays/triangularSum.py
@@ -17,6 +17,26 @@
 # Repeat the entire process starting from step 1.
 # Return the triangular sum of nums.
 
+# Example 1:
+
+
+# Input: nums = [1,2,3,4,5]
+# Output: 8
+# Explanation:
+# The above diagram depicts the process from which we obtain the triangular sum of the array.
+# Example 2:
+
+# Input: nums = [5]
+# Output: 5
+# Explanation:
+# Since there is only one element in nums, the triangular sum is the value of that element itself.
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 1000
+# 0 <= nums[i] <= 9
+
 class Solution:
     def triangularSum(self, nums: List[int]) -> int:
         n = len(nums)
