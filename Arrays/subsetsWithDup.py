d97959530e89c1ff019e9e59280de12b2d4fb0d2
Shraddha Piparia <shraddhapiparia@gmail.com>
2025-02-20 09:50:19 -0800

    Create subsetsWithDup.py

---
+++ Arrays/subsetsWithDup.py
@@ -0,0 +1,28 @@
+# 90. Subsets II Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+# The solution set must not contain duplicate subsets. Return the solution in any order.
+
+# Example 1:
+# Input: nums = [1,2,2]
+# Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+
+# Example 2:
+# Input: nums = [0]
+# Output: [[],[0]]
+ 
+# Constraints:
+# 1 <= nums.length <= 10
+# -10 <= nums[i] <= 10
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()  # sort to group duplicates together
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        res.append(path)
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue  # skip duplicates
+            self.dfs(nums[i+1:], path + [nums[i]], res)
